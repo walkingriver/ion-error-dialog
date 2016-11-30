8707da0f7865ffcd66c5df8bff1d229351fbbe2c
Mike Callaghan <Michael.D.Callaghan@Disney.com>
2016-11-30 08:23:44 -0500

    Minor tweaks.

--- www/js/error-dialog.js
+++ www/js/error-dialog.js
@@ -3,46 +3,71 @@
         .component('errorDialog', {
             bindings: {
                 errorMessage: '<',
-                isVisible: '<'
+                isVisible: '<',
+                onClosed: '&'
             },
             controller: ErrorDialogController
         });
 
     var modalTemplate = `
-    <ion-modal-view>
-    <ion-header-bar>
-      <h1 class="title">{{title}}</h1>
-    </ion-header-bar>
-    <ion-content>
-        <h2>There was a nasty error</h2>
-      {{errorMessage}}
-    </ion-content>
-    <ion-footer-bar>
-        <button button button-block ng-click="closeModal()">Close</button>
-    </ion-footer-bar>
-    </ion-modal-view>`;
+<ion-modal-view class="error-header-txt" view-title="System Error">
+  <ion-header-bar class="error-padding">
+    <h4>System Error</h4>
+  </ion-header-bar>
+  <ion-content class="has-footer has-header">
+    <div class="error-b2 content-border">
+      <div class="row">
+        <div class="col">{{errorMessage}}</div>
+      </div>
+    </div>
+  </ion-content>
+  <div class="bar bar-footer">
+    <button class="btn-wdpr-secondary error-buttons" ng-click="closeModal()">OK</button>
+  </div>
+</ion-modal>
+    `;
 
     function ErrorDialogController($ionicModal, $rootScope) {
         var vm = this;
-        var scope = $rootScope.$new();
         vm.$onChanges = onChanges;
+        vm.$onInit = onInit;
         vm.isVisible = false;
-
-        scope.closeModal = closeModal;
-        scope.errorMessage = vm.errorMessage;
-
-        vm.modal = $ionicModal.fromTemplate(modalTemplate, {
-            scope: scope,
-            animation: 'slide-in-up'
-        });
 
         function openModal() {
             vm.modal.show();
         };
+
         function closeModal() {
             vm.isVisible = false;
             vm.modal.hide();
         };
+
+        function onInit() {
+            var scope = $rootScope.$new();
+            scope.closeModal = closeModal;
+            scope.errorMessage = vm.errorMessage;
+
+            vm.modal = $ionicModal.fromTemplate(modalTemplate, {
+                scope: scope,
+                animation: 'slide-in-up'
+            });
+
+            // Cleanup the modal when we're done with it!
+            scope.$on('$destroy', function () {
+                vm.modal.remove();
+            });
+            // Execute action on hide modal
+            scope.$on('modal.hidden', function () {
+                // Execute action
+                if (vm.onClosed) {
+                    vm.onClosed();
+                }
+            });
+            // Execute action on remove modal
+            scope.$on('modal.removed', function () {
+                // Execute action
+            });
+        }
 
         function onChanges(changes) {
             if (changes.isVisible) {
@@ -54,17 +79,5 @@
             }
         }
 
-        // Cleanup the modal when we're done with it!
-        scope.$on('$destroy', function () {
-            vm.modal.remove();
-        });
-        // Execute action on hide modal
-        scope.$on('modal.hidden', function () {
-            // Execute action
-        });
-        // Execute action on remove modal
-        scope.$on('modal.removed', function () {
-            // Execute action
-        });
     }
 })();
