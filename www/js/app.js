36eb248ba24936dc56c3421ba0704acb61770f43
Mike Callaghan <Michael.D.Callaghan@Disney.com>
2016-11-30 09:03:02 -0500

    Added alternative error service.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'errorDialog'])
+angular.module('starter', ['ionic', 'errorDialog', 'errorModule'])
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -21,13 +21,20 @@ angular.module('starter', ['ionic', 'errorDialog'])
       }
     });
   })
-  .controller('AppController', function () {
+  .controller('AppController', function (errorDialogService) {
     var vm = this;
     vm.errorMessage = '';
+
+    // Component
     vm.showErrorDialog = function () {
       vm.errorMessage = 'Mike was here - again';
     };
     vm.onClosed = function () {
       vm.errorMessage = '';
+    }
+
+    // Service
+    vm.showError = function () {
+      errorDialogService.showError('Mike used his service correctly.');
     }
   });
